72a5cbf1a73793c10f8a1b359466eb02823b1559
Stanly Anasi <anasistanly@gmail.com>
2024-09-21 16:56:59 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,6 +1,12 @@
-from app import create_app
+from flask import Flask
 
-app = create_app()
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('config.Config')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+    from .routes import main
+    app.register_blueprint(main)
+
+    # Register other blueprints if needed
+
+    return app
