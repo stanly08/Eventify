4450bff9313618ecc49f000cda8afe4c81966477
Stanly Anasi <anasistanly@gmail.com>
2024-11-24 02:08:53 +0300

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,9 @@
 from flask import render_template
-from app import app
+from app import app, db
+from app.models import Event
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    events = Event.query.all()
+    return render_template('event_list.html', events=events)
