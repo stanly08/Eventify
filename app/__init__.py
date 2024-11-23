faed64ad898684fd67d6b7d356870fdf2a42d2d7
Stanly Anasi <anasistanly@gmail.com>
2024-11-24 01:25:22 +0300

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object('config')
+print("Database URI: ", app.config['SQLALCHEMY_DATABASE_URI'])
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
