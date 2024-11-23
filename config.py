b5b72f7cede2449595015b97c348ea9aa5d64e40
stanly08 <anasistanly@gmail.com>
2024-11-24 01:40:13 +0300

    delete

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///events.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = 'app/static/uploads'
-
-print("Database URI from config:", os.environ.get('DATABASE_URL'))
