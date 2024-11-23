7116dac96d8b4f794deeaa008a49f860367bee61
Stanly Anasi <anasistanly@gmail.com>
2024-11-24 02:25:13 +0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,21 +1,29 @@
 from app import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
-    
+    password_hash = db.Column(db.String(128), nullable=False)
 
-class Admin(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(150), unique=True, nullable=False)
-    email = db.Column(db.String(150), unique=True, nullable=False)
-    
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+class Admin(User):
+    __tablename__ = 'admin'
+    id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    is_superadmin = db.Column(db.Boolean, default=False)
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(200), nullable=False)
     date = db.Column(db.Date, nullable=False)
     location = db.Column(db.String(200), nullable=False)
-    photo = db.Column(db.String(200))  # field for event photo filename
+    photo = db.Column(db.String(200)) 
+
     
