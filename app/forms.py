76e2f14654f9703c103511fe63b45a2625f01f63
Stanly Anasi <anasistanly@gmail.com>
2024-11-27 16:48:32 +0300

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,18 +1,18 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField, SelectField
-from wtforms.validators import DataRequired
+from wtforms import StringField, DateField, FileField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo
+
+class SignupForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
-
-class SignupForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    role = SelectField('Role', choices=[('user', 'User'), ('admin', 'Admin')], validators=[DataRequired()])
-    submit = SubmitField('Sign Up')
 
 class EventForm(FlaskForm):
     name = StringField('Event Name', validators=[DataRequired()])
@@ -20,4 +20,5 @@ class EventForm(FlaskForm):
     location = StringField('Location', validators=[DataRequired()])
     photo = FileField('Event Photo')
     submit = SubmitField('Add Event')
+
 
